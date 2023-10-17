dee684ae82c27c7d402b937219b6ce1c013aa012
Chaimaa-H <professionalchou52@gmail.com>
2023-10-17 16:28:07 -0700

    0

--- main.h
+++ main.h
@@ -1,40 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* header files */
 #include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-
-/**
- * struct format - Struct for format
- * @ch: Struct format
- * @f: The function associated
- */
-
-typedef struct format
-{
-	char ch;
-	int (*f)(va_list);
-} format_t;
-
-void print_Hexadecimal(unsigned int num, int *p);
-int print_ex_str(va_list ap);
-int print_hex_ptr(va_list ap);
-int print_address(va_list ap);
-int print_binary(va_list ap);
-int print_unsigned(va_list ap);
-int print_octal(va_list ap);
-int print_hex(va_list ap);
-int print_Hex(va_list ap);
-int reverse_string(va_list ap);
-int len_int(int ap);
-int _printf(const char *format, ...);
+/* function prototypes */
 int _putchar(char c);
-int print_char(va_list ap);
-int print_mod(va_list ap);
-int print_int(va_list ap);
-int print_string(va_list ap);
-
+int _printf(const char *format, ...);
+int _puts(const char *str);
+int print_num(int n);
+void specifiers(va_list par_list, const char *format, int *i, int *count);
+int morespec(va_list par, const char *fmt, int *i, int *count);
+int _revputs(const char *str);
+void print_width_int(char c, int n, int *counter);
+int _isdigit(char c);
+int print_binary(unsigned int n);
+void print_null_str(const char *par_str, int *count);
+int print_unsignedd(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex(unsigned int n, int uppercase);
+int u_specifier(va_list par, int *count);
+int r_specifier(va_list par, int *count);
+int b_specifier(va_list par, int *count);
+int o_specifier(va_list par, int *count);
+int x_specifier(va_list par, int *count);
+int X_specifier(va_list par, int *count);
+int S_specifier(va_list par, int *count);
+int print_custom_string(const char *str);
 #endif
--- printf0.c
+++ printf0.c
@@ -1,122 +1,34 @@
 #include "main.h"
 
-int handle_hash(char c, int *i);
 /**
- * handle_plus_space - handle plus and spaces after % sign
- * @c: the char after %
- * @num: number that will be printed after
- * Return: len of printed char
- */
-int handle_plus_space(char c, int num)
-{
-	int len = 0;
-
-	if (c == '+')
-	{
-		int check_isNeg;
-
-		check_isNeg = num > 0 ? 0 : 1;
-		if (!check_isNeg)
-		{
-			_putchar('+');
-			len++;
-		}
-	}
-	else
-	{
-		_putchar(' ');
-		len++;
-	}
-	return (len);
-}
-
-/**
- * _printf - print f function
- * @format: the string will be printed
- * Return: number of digits printed
+ * _printf - custom printf function
+ * desc: acts as printf
+ * @format: format string to be outputed
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int j, i = 0, len = 0;
-	va_list ap;
+	int i, count = 0;
+	va_list par_list;
 
-	format_t fun[] = {{'c', print_char}, {'s', print_string},
-		{'%', print_mod}, {'i', print_int},
-		{'d', print_int}, {'r', reverse_string},
-		{'x', print_hex}, {'X', print_Hex},
-		{'o', print_octal}, {'u', print_unsigned},
-		{'b', print_binary}, {'p', print_address},
-		{'S', print_ex_str}
-	};
+	va_start(par_list, format);
 
-	va_start(ap, format);
-
-	if ((!format) || (format[0] == '%' && !format[1]))
+	if (format == NULL)
 		return (-1);
-
-	while (format[i])
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
+			i++;
+			if (format[i] == '\0')
 				return (-1);
-			if (format[i + 1] == '+' || format[i + 1] == ' ')
-			{
-				va_list dest;
-
-				va_copy(dest, ap);
-				len += handle_plus_space(format[i + 1], va_arg(dest, int));
-				i++;
-			}
-			else if (format[i + 1] == '#')
-			{
-				len += handle_hash(format[i + 2], &i);
-			}
-			j = 0;
-			while (j < 13)
-			{
-				if (format[i + 1] == fun[j].ch)
-				{
-					len += fun[j].f(ap);
-					i += 2;
-					break;
-				}
-				j++;
-			}
+			specifiers(par_list, format, &i, &count);
 		}
 		else
 		{
-			_putchar(format[i]);
-			len++;
-			i++;
-
+			count += _putchar(format[i]);
 		}
 	}
-	va_end(ap);
-	return (len);
-}
-/**
- * handle_hash - handle after the hash sign
- * @c: the char after # sign
- * @i: pointer to current char of @format
- * Return: len of printed char
- */
-int handle_hash(char c, int *i)
-{
-	int len = 0;
-
-	if (c == 'x' || c == 'X')
-	{
-		_putchar('0');
-		_putchar('x');
-		len += 2;
-		(*i)++;
-	}
-	else if (c == 'o')
-	{
-		_putchar('0');
-		len++;
-		(*i)++;
-	}
-	return (len);
+	va_end(par_list);
+	return (count);
 }
